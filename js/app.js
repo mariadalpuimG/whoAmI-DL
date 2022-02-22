cf7df188748aac149611e3f69a0d62e66915bb87
mariadalpuimG <79101889+mariadalpuimG@users.noreply.github.com>
2022-02-22 22:33:36 +0000

    commit to not lose anything else

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// // open and close rules buttons
+
+// console.log(document.getElementById('rulesContainer'))
+
+// const btnStart = document.getElementById('rulesContainer');
+
+// openRules = () => {
+//     btnStart.style.display = "block";
+// };
+
+// closeRules = () => {
+//     btnStart.style.display = "none";
+// };
+
+// document.getElementById('btnStart').addEventListener('click', openRules)
+// document.getElementById('btnClose').addEventListener('click', closeRules)
